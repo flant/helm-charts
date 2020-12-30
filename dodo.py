c04c75b8c7c2c39c36683368c02e8e07491c0e4c
Ilya Lesikov <ilya@lesikov.com>
2020-12-30 14:07:52 +0300

    cleanup CI

--- dodo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from doit.cmd_base import ModuleTaskLoader
-from doit.doit_cmd import DoitMain
-
-import werf_chart_repo_doit_tasks.tasks
-sys.exit(DoitMain(ModuleTaskLoader(werf_chart_repo_doit_tasks.tasks)).run(sys.argv[1:]))
